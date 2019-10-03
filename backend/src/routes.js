a1f9a0494df6728675712c29153dbb88d96a45db
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-03 11:21:57 +0100

    show user spot

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require("./config/upload");
 
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -13,5 +14,7 @@ routes.post("/sessions", SessionController.store);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 routes.get("/spots", SpotController.index);
+
+routes.get("/dashboard", DashboardController.show);
 
 module.exports = routes;
