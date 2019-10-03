da3eaa90449b39617bedc8c6c7ac1fdb031377d7
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-03 18:30:16 +0100

    create main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import logo from "./assets/logo.svg";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre
+          <strong> talentos</strong> para a sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu email aqui" />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
