d402cd6016b10866f8987e0f8bf375ce1aa25f38
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-03 10:48:51 +0100

    create spot database

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,7 +1,25 @@
+const Spot = require("../models/Spot");
+const User = require("../models/User");
+
 module.exports = {
   async store(req, res) {
-    console.log(req.body);
-    console.log(req.file);
-    return res.json({ ok: true });
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User does not exist" });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(",").map(tech => tech.trim()),
+      price
+    });
+    return res.json(spot);
   }
 };
