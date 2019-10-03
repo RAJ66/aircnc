9682a3237d2c2e7fe048729a69a06251dd46cece
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-03 10:54:54 +0100

    listing spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,4 +12,6 @@ routes.post("/sessions", SessionController.store);
 
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
+routes.get("/spots", SpotController.index);
+
 module.exports = routes;
